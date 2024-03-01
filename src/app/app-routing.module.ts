2408ed249198bf3ab5584293c7865b65fde090be
Denys1994q <drybachok@gmail.com>
2024-03-01 12:08:30 +0200

    add error route component for unmentioned routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './services/auth.guard';
 import { AuthComponent } from './auth/auth.component';
-import { AdminSectionModule } from './admin-section/admin-section.module';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
     { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
     { path: 'auth', component: AuthComponent },
-    {path: 'admin-section', loadChildren: () => import('./admin-section/admin-section.module').then((m) => m.AdminSectionModule)},
+    { path: 'admin-section', 
+        loadChildren: () => import('./admin-section/admin-section.module').then((m) => m.AdminSectionModule)},
+    {path: 'error', component: ErrorComponent},
+    {path: '**', redirectTo: '/error'},
 ];
 
 @NgModule({
