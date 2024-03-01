01b0785fe5d362a81a329103a7a6243b440b81e3
Denys1994q <drybachok@gmail.com>
2024-03-01 12:04:46 +0200

    add lazy module for admin-section

--- src/app/admin-section/admin-section.module.ts
+++ src/app/admin-section/admin-section.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 import { TableModule } from '../table/table.module';
 import { AdminSectionComponent } from './admin-section.component';
 import { ErrorModule } from '../error/error.module';
+import { AuthGuard } from '../services/auth.guard';
+import { RouterModule } from '@angular/router';
+
+const routes = [
+    {path: '', component: AdminSectionComponent, canActivate: [AuthGuard]}
+]
 
 @NgModule({
     declarations: [
         AdminSectionComponent
     ],
     imports: [
+        RouterModule.forChild(routes),
         CommonModule,
         TableModule,
         ErrorModule
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './services/auth.guard';
 import { AuthComponent } from './auth/auth.component';
-import { AdminSectionComponent } from './admin-section/admin-section.component';
+import { AdminSectionModule } from './admin-section/admin-section.module';
 
 const routes: Routes = [
     { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
     { path: 'auth', component: AuthComponent },
-    { path: 'admin-section', component: AdminSectionComponent, canActivate: [AuthGuard]}
+    {path: 'admin-section', loadChildren: () => import('./admin-section/admin-section.module').then((m) => m.AdminSectionModule)},
 ];
 
 @NgModule({
